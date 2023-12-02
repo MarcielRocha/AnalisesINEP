d3332c5c9ea76c4718d721f2cff2cd5f5d2e84bd
Marciel Rocha <103228009+MarcielRocha@users.noreply.github.com>
2023-12-02 12:08:30 -0300

    Add files via upload
    Script para criação de arquivos com dados do Censo Escolar do INEP apenas da cidade de Campinas nos anos de 2022, 2021, 2020 e 2019.

---
+++ Criacao Data Frame Campinas.R
@@ -0,0 +1,27 @@
+# Processo de seleção dos dados do Censo Escolar apenas da região de Campinas
+# Alterar para a pasta onde se encontram os arquivos
+setwd("C:/Users/marci/OneDrive/Documentos/Monografia IFSP/Censo Escolar/Dados") 
+getwd()
+
+# Segmentação dos arquivos com dados apenas da cidade de Campinas
+censo <- data.frame(read.csv("microdados_ed_basica_2022.csv", header = TRUE, sep = ";"))
+censo_campinas_2022 <- subset.data.frame(censo, NO_MUNICIPIO == 'Campinas')
+
+censo <- data.frame(read.csv("microdados_ed_basica_2021.csv", header = TRUE, sep = ";"))
+censo_campinas_2021 <- subset.data.frame(censo, NO_MUNICIPIO == 'Campinas')
+
+censo <- data.frame(read.csv("microdados_ed_basica_2020.csv", header = TRUE, sep = ";"))
+censo_campinas_2020 <- subset.data.frame(censo, NO_MUNICIPIO == 'Campinas')
+
+censo <- data.frame(read.csv("microdados_ed_basica_2021.csv", header = TRUE, sep = ";"))
+censo_campinas_2019 <- subset.data.frame(censo, NO_MUNICIPIO == 'Campinas')
+
+
+# Criação dos arquivos com dados apenas da cidade de Campinas
+write.csv(censo_campinas_2022,"microdados_campinas_2022.csv", row.names = FALSE)
+write.csv(censo_campinas_2021,"microdados_campinas_2021.csv", row.names = FALSE)
+write.csv(censo_campinas_2020,"microdados_campinas_2020.csv", row.names = FALSE)
+write.csv(censo_campinas_2019,"microdados_campinas_2019.csv", row.names = FALSE)
+
+
+
